4f449b36b18f6500d3711cf059de471e52436edc
kjs333 <rlawltjsrla@gmail.com>
2025-11-11 12:27:25 +0900

    weather model 값이 null일 경우 추가

--- lib/ui/pages/home/widgets/home_page_btm_navigation.dart
+++ lib/ui/pages/home/widgets/home_page_btm_navigation.dart
@@ -20,14 +20,6 @@ class HomePageBtmNavigation extends ConsumerWidget {
         child: Center(child: CircularProgressIndicator()),
       ),
       data: (data) {
-        if (data == null) {
-          return Container(
-            width: double.infinity,
-            height: 100,
-            color: vrc(context).generalBack,
-          );
-        }
-        final year = data.time.year % 100;
         return Container(
           width: double.infinity,
           height: 100,
@@ -48,33 +40,40 @@ class HomePageBtmNavigation extends ConsumerWidget {
                 ),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    spacing: 10,
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Text(
-                              textAlign: TextAlign.end,
-                              "업데이트 시간 : $year년 ${data.time.month}월 ${data.time.day}일 ${data.time.hour}시 ${data.time.minute}분",
+                child: data == null
+                    ? Container(
+                        alignment: Alignment.topCenter,
+                        width: 50,
+                        height: 50,
+                        child: Text("잠시 후 다시 새로고침해주세요."),
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          spacing: 10,
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    textAlign: TextAlign.end,
+                                    "업데이트 시간 : ${data.time.year % 100}년 ${data.time.month}월 ${data.time.day}일 ${data.time.hour}시 ${data.time.minute}분",
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                (data.time.hour >= 6 && data.time.hour <= 18)
+                                    ? Icon(Icons.sunny)
+                                    : Icon(Icons.nightlight_round),
+                              ],
                             ),
-                          ),
-                          SizedBox(width: 10),
-                          (data.time.hour >= 6 && data.time.hour <= 18)
-                              ? Icon(Icons.sunny)
-                              : Icon(Icons.nightlight_round),
-                        ],
+                            Text(
+                              textAlign: TextAlign.end,
+                              '날씨:${data.weatherDescription} 온도:${data.temperature}°C  풍속:${data.windSpeed}m/s',
+                            ),
+                          ],
+                        ),
                       ),
-                      Text(
-                        textAlign: TextAlign.end,
-                        '날씨:${data.weatherDescription} 온도:${data.temperature}°C  풍속:${data.windSpeed}m/s',
-                      ),
-                    ],
-                  ),
-                ),
               ),
             ],
           ),
